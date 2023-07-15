a35ee5781314db104ea5d3dd0c8980f20661d5cd
Syed Mohammad Yasir <smy329@gmail.com>
2023-07-15 18:34:32 +0600

    navbar

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,9 +2,14 @@
 import { CustomButtonProps } from '@/types';
 import React from 'react';
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, btnType, handleClick }: CustomButtonProps) => {
   return (
-    <button disabled={false} type={'button'} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+    <button
+      disabled={false}
+      type={btnType || 'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
       <span className={`flex-1`}>{title}</span>
     </button>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import CustomButton from './CustomButton';
 
 const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
-      <nav className="max-w-[1440px] mx-auto flex justify-between items-center px-6 sm:px-16 py-4"></nav>
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center px-6 sm:px-16 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image src="/logo.svg" alt="drivelux logo" width={200} height={50} className="object-contain" />
+        </Link>
+        <CustomButton
+          title="Sign In"
+          btnType="button"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
+      </nav>
     </header>
   );
 };
